39b6d58ba52234255c7d8611af7fe61e4abf18fa
jcamilo.coru <juancamilo@coruapp.com>
2020-06-28 15:25:56 -0500

    optimizing datastructures with entities

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -2,13 +2,13 @@ import * as fromPizzas from '../actions/pizzas.action';
 import { Pizza } from '../../models/pizza.model';
 
 export interface PizzaState {
-  data: Pizza[],
+  entities: { [id: number]: Pizza },
   loaded: boolean,
   loading: boolean
 };
 
 export const initialState: PizzaState = {
-  data: [],
+  entities: {},
   loaded: false,
   loading: false
 };
@@ -18,16 +18,27 @@ export function reducer(state = initialState, action: fromPizzas.PizzasAction): 
   switch (action.type) {
     case fromPizzas.LOAD_PIZZAS:
       return { ...state, loading: true, loaded: false };
+      
     case fromPizzas.LOAD_PIZZAS_SUCCESS:
-      const data = action.payload;
-      return { ...state, data, loading: false, loaded: true };
+      const pizzas = action.payload;
+      const entities = pizzas.reduce((entities: {[id: number]: Pizza}, pizza: Pizza) => {
+        return {
+          ...entities,
+          [pizza.id]: pizza
+        }
+      }, {
+        ...state.entities
+      });
+      return { ...state, entities, loading: false, loaded: true };
+
     case fromPizzas.LOAD_PIZZAS_FAIL:
       return { ...state, loading: false, loaded: false };
+      
     default:
       return state;
   }
 }
 
+export const getPizzasEntities = (state: PizzaState) => state.entities;
 export const getPizzasLoading = (state: PizzaState) => state.loading;
 export const getPizzasLoaded = (state: PizzaState) => state.loaded;
-export const getPizzas = (state: PizzaState) => state.data;
