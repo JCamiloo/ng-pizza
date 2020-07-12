da78d560f2a7d29ba518e7528578d50720dcb514
JCamiloo <jcamilo.osorio15@gmail.com>
2020-07-11 23:35:08 -0500

    Dispatch Routing actions, combine ofType for single effect

--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Effect, Actions } from '@ngrx/effects';
 import { map, switchMap, catchError } from 'rxjs/operators';
 import { of } from 'rxjs/observable/of';
+import * as fromRoot from '../../../app/store';
 import * as pizzaActions from '../actions';
 import * as fromServices from '../../services';
 
@@ -32,6 +33,17 @@ export class PizzasEffects {
   );
 
   @Effect()
+  createPizzaSuccess$ = this.actions$.ofType(pizzaActions.CREATE_PIZZA_SUCCESS).pipe(
+    map((action: pizzaActions.CreatePizzaSuccess) => action.payload),
+    map(pizza => {
+      return new fromRoot.Go({
+        // path: ['/products', pizza.id],
+        path: ['/products'],
+      });
+    })
+  );
+
+  @Effect()
   updatePizza$ = this.actions$.ofType(pizzaActions.UPDATE_PIZZA).pipe(
     map((action: pizzaActions.UpdatePizza) => action.payload),
     switchMap(pizza => {
@@ -52,4 +64,18 @@ export class PizzasEffects {
       );
     })
   );
+
+  @Effect()
+  handlePizzaSuccess$ = this.actions$
+    .ofType(
+      pizzaActions.UPDATE_PIZZA_SUCCESS,
+      pizzaActions.REMOVE_PIZZA_SUCCESS
+    )
+    .pipe(
+      map(pizza => {
+        return new fromRoot.Go({
+          path: ['/products'],
+        });
+      })
+    );
 }
